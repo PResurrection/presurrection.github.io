8aafbb7e8f636a47d55959556461abbc5f798e91
PResurrection <pavel_zakr@mail.ru>
2023-04-11 19:47:12 +0500

    Try 3

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="page-footer">
-            <div className="container">
-                © {new Date().getFullYear()} All rights reserved
-            </div>
-        </footer>
-    );
-}
